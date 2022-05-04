4144694b521649c5994ab670d25911ac33a60a97
Cristian Florez <cristian.florez@gmail.com>
2022-05-04 13:57:06 -0500

    interaccion html y js

--- figuras.js
+++ figuras.js
@@ -26,7 +26,7 @@ console.group("Triangulos")
 //const alturaTriagulo=5.5;
 
 function perimetroTriangulo(lado1, lado2, base){
-    return lado1+lado2+base;
+    return Number(lado1+lado2+base);
 };
 
 function areaTriangulo(base, altura){
@@ -44,3 +44,45 @@ console.group("circulo")
 console.log("luego en el circulo usabamos Math.pi: "+Math.PI);
 
 console.groupEnd();
+
+function calcularPerimetroCuadrado(){
+    const input=document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const perimetro=perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const input=document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area=areaCuadrado(value);
+    alert(area);
+}
+
+function calcularPerimetroTriangulo(){
+    const input1=document.getElementById("inputLado1");
+    const lado1=Number(input1.value);
+
+    const input2=document.getElementById("inputLado2");
+    const lado2=Number(input2.value);
+
+    const inputbase=document.getElementById("inputBase");
+    const base=Number(inputbase.value);
+
+    const perimetro=perimetroTriangulo(lado1,lado2,base);
+    alert(perimetro);
+
+}
+
+function calcularAreaTriangulo(){
+    const inputbase=document.getElementById("inputBase");
+    const base=Number(inputbase.value);
+
+    const inputAltura=document.getElementById("inputAltura");
+    const altura=Number(inputAltura.value);
+
+    const area=Number(areaTriangulo(base,altura));
+    alert(area);
+}
